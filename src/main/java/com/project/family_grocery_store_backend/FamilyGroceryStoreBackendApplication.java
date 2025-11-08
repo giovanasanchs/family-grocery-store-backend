5222439a1e676999819b1aac578a4ed151a646b6
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-11-07 23:37:50 -0300

    :tada: project created

---
+++ src/main/java/com/project/family_grocery_store_backend/FamilyGroceryStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.family_grocery_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyGroceryStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyGroceryStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/family_grocery_store_backend/FamilyGroceryStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.family_grocery_store_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyGroceryStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
